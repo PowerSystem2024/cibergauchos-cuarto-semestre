fcf1b196bccbf81418857a6899167cbe29d0b4ca
Nahuel Molino <nahuel.molino.mza@gmail.com>
2025-08-19 20:41:26 -0300

    Modifico archivo de prueba

--- python/Clase01/LaboratorioUsuario/laboratorioUsuario/tests/test_usuario.py
+++ python/Clase01/LaboratorioUsuario/laboratorioUsuario/tests/test_usuario.py
@@ -1,8 +1,10 @@
 from Usuario import Usuario
 
 def prueba_basica():
-        u1=Usuario(1,"nahuel", "molino")
-        print(u1)
+    u1=Usuario(1,"nahuel", "molino")
+    u2= Usuario(2, "norma", "liliana")
+    print(u1)
+    print(u2)
 
 prueba_basica()
 
