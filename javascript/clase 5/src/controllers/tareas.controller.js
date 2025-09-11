61133066391e6a37e9ca9398c47f77320b9b2698
Danilo Pietropaolo <francodanilo136@gmail.com>
2025-09-11 19:39:25 -0300

    clase 5 javascript

---
+++ javascript/clase 5/src/controllers/tareas.controller.js
@@ -0,0 +1,11 @@
+export const listarTareas = (req, res) => {
+  res.send("Obteniendo tareas...");
+};
+
+export const listarTareaId = (req, res) => res.send("Obteniendo tarea única");
+
+export const crearTarea = (req, res) => res.send("Creando tarea");
+
+export const actualizarTarea = (req, res) => res.send("Actualizando tarea");
+
+export const eliminarTarea = (req, res) => res.send("Eliminando tarea...");
