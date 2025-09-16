ab6caac39e3b92c31cf79f74df29c5653dbc9828
Franco Danilo Pietropaolo <140764604+FrancoDaniloP@users.noreply.github.com>
2025-09-16 20:14:47 -0300

    Clase 5 de javascript
    * clase 1 python
    
    * gitignore agregado
    
    * Carpeta de pycache eliminada
    
    * archivo gitignore agregado
    
    * archivo log eliminado
    
    * clase 5 javascript

---
+++ javascript/clase 5/src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import morgan from "morgan";
+import tareasRoutes from "./router/tareas.routes.js";
+import authRoutes from "./router/auth.routes.js";
+
+const app = express();
+
+//Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send({ message: "Bienvenidos" });
+});
+app.use("/api", tareasRoutes);
+app.use("/api", authRoutes);
+
+//Manejo de errores
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
+export default app;
