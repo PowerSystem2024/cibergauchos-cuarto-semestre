4d27751664ce9c918326aff88b65f4f63386ee38
Micaela <micaelagonsalez20@gmail.com>
2025-09-24 20:10:17 -0300

    Clase 03 Python Profundizando con Sistemas Numéricos y String-> Parte 2  (#33)
    * clase 1 javascript
    
    * clase 3 de python
    
    * corrección de carpeta

---
+++ python/clase 3/tipo_str.py
@@ -0,0 +1,10 @@
+import math
+# Profundizando en el tipo String
+# Concatenación automática en Python
+
+variable = ' Chau'
+mensaje = 'Hola ''Profesor' + variable
+mensaje += ', Asta Luego'
+print(mensaje)
+# Usamos la clase help para ayudar o documentación (built-in)
+help(math.isnan)
