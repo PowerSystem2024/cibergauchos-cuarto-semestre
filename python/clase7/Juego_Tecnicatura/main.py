f33a756836d1f43982f70f9651a590234e0fb2a7
Ulises Santiago Sánchez <ulisessanchezcarp2004@gmail.com>
2025-10-01 21:02:20 -0300

    Clase 7 Python

---
+++ python/clase7/Juego_Tecnicatura/main.py
@@ -0,0 +1,6 @@
+import pygame
+import sys
+import random
+import os
+from personaje import Personaje, Enemigo, Explosion
+from constantes import ANCHO_PANTALLA, ALTO_PANTALLA, FPS, COLOR_FONDO, COLOR_BARRA_VIDA, COLOR_BARRA_VIDA_FONDO
