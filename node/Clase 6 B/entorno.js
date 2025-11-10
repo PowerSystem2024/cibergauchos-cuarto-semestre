2038c791a69ec532badf73121fd6a06a023fee1f
Marquez Luciano <127359173+Luc14n088@users.noreply.github.com>
2025-11-10 09:12:53 -0300

    Clases Node y JavaScript
    * Clase 1 Ecommerce
    
    * Clase 2 Ecommerce
    
    * Clase 3 Ecommerce
    
    * Clase 2 Node
    
    * Clase 4 Ecommerce
    
    * Clases 3, 5, 6 Node
    
    * Clase 12 Javascript - Proyecto Bomba
    
    * Clase 6 B Node

---
+++ node/Clase 6 B/entorno.js
@@ -0,0 +1,8 @@
+
+let nombre = process.env.NOMBRE || 'Sin Nombre';
+let web = process.env.WEB || 'No tengo Web';
+
+console.log('Hola '+ nombre);
+console.log('Mi web es '+ web);
+
+console.log('Ultima ejecución');
