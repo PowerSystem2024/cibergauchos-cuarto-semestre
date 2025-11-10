f7239619d43748e1d59d8fc53bd7e0525720cd90
leonela <leonelahuanca1@gmail.com>
2025-11-10 10:04:07 -0300

    Agrego Node Clase 12 Modulos -> File System

---
+++ node/02-Fundamentos_node/modulos/fs.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+//Primero leemos el archivo.txt
+function leer(ruta, cb) {
+    fs.readFileSync(ruta, (err, data) => {
+    cb(data.toString());
+    })
+}
+
+leer(`${__dirname}/archivo.txt`, console.log); // Sintaxis ES6
+
+//Segundo escribimos el archivo.txt creandolo
+function escribir(ruta, contenido, cb) {
+    fs.writeFile(ruta, contenido, function (err){
+        if (err) {
+            console.log('No se ha podido escribir', err);   
+        } else {
+            console.log('Se ha escrito correctamente');
+        }
+    })
+}
+
+
+//Tercero eliminamos el archivo1.txt
+function borrar(ruta, cb){
+    fs.unlink(ruta, cb); //elimina de manera asincrona
+}
+
+borrar(`${__dirname}/archivo1.txt`, console.log);
+//escribir(`${__dirname}/archivo1.txt`, 'Reescribimos el archivo');
+//leer(`${__dirname}/archivo.txt`, console.log);
